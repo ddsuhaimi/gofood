ef8e0093303c28c36faca09d685aec20445b4062
ddsuhaimi <dedisuhaimiacc@gmail.com>
2024-12-22 15:01:17 +0700

    tes

--- main.go
+++ main.go
@@ -2,14 +2,21 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"os"
 )
 
 func main() {
 	router := gin.Default()
 	router.GET("/", func(c *gin.Context) {
+		// Retrieve the hostname of the server
+		hostname, err := os.Hostname()
+		if err != nil {
+			hostname = "unknown"
+		}
+
 		c.JSON(200, gin.H{
-			"message": "Hello, World!",
-			"hostname": c.Request.Host,
+			"message":  "Hello, World!",
+			"hostname": hostname,
 		})
 	})
 	router.Run(":8080")
